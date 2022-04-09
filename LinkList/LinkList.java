0530576e26f738e18df06f0868a4864de06a5a2b
Kethmi Soysa <91780060+Kethmi92@users.noreply.github.com>
2022-04-09 23:59:08 +0530

    Create LinkList file

---
+++ LinkList/LinkList.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class LinkedListEx{
+  public static void main (String args[]){
+
+     LinkedList<school> = new LinkedList<String>();
+      school.add("SSGS");
+      school.add("PBMV");
+      school.add("SSC");	
+
+System.out.println(school);
+   }
+
+}
